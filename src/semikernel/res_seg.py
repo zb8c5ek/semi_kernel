4647cd713e64c6fcef5a8d47af9753786bb0c9f7
xuchen <flora1011>
2018-07-10 12:29:40 +0200

    otsu thresholding accomplished, awaits test

--- src/semikernel/res_seg.py
+++ src/semikernel/res_seg.py
@@ -1,4 +1,5 @@
 import torch as pt
+import numpy as np
 
 """
 Xuanli Chen
@@ -19,5 +20,45 @@ LinkedIn: https://be.linkedin.com/in/xuanlichen
 
 def otsu_thresholding(img, bins=20):
     """"""
-    b = pt.histc(input=img, bins=bins, min=0, max=0, out=None)
+
+    img_min = pt.min(img)   # without dimension, 'min' function searches for the global minimum. maximum the same
+    img_max = pt.max(img)
+
+    b = pt.histc(input=img, bins=bins, min=img_min, max=img_max, out=None)
+    b_value = pt.linspace(img_min, img_max, step=bins)
     p = b / pt.sum(b)
+
+    omega_0 = pt.FloatTensor(bins).fill_(0)
+    omega_1 = pt.FloatTensor(bins).fill_(0)
+
+    miu_0 = pt.FloatTensor(bins).fill_(0)
+    miu_1 = pt.FloatTensor(bins).fill_(0)
+
+    # -------------------- #
+
+    omega_0[0] = p[0]
+    omega_1[0] = pt.sum(p) - p[0]
+
+    miu_0[0] = b_value[0] * p[0]    # miu value is without divide over omega
+    miu_1[0] = pt.sum(b_value[1:]*p[1:])
+
+    for t in np.arange(1, bins):
+
+        omega_0[t] = omega_0[t-1] + p[t]
+        omega_1[t] = omega_1[t-1] - p[t]
+
+        miu_0[t] = miu_0[t-1] + b_value[t] * p[t]
+        miu_1[t] = miu_1[t-1] - b_value[t] * p[t]
+
+    miu_0 = miu_0 / omega_0
+    miu_1 = miu_1 / omega_1
+
+    theta_b = (miu_0 - miu_1).pow(2) * omega_0 * omega_1
+
+    thres = b_value[pt.argmax(theta_b)]
+
+    return thres
+
+
+
+
